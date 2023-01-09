559d273f19f99e43726b9d9ab2bded22e5e8b9d5
MarcosPauloRamos <49656714+MarcosPauloRamos@users.noreply.github.com>
2023-01-09 16:22:47 -0300

    Update bios.c

--- compilador/os/bios.c
+++ compilador/os/bios.c
@@ -14,7 +14,6 @@ void checarComponentes(void) {
 
 /**
  * Transfere as instruções referentes ao SO do HD para a memória de instruções.
- * Retorna a posição da última instrução copiada do HD.
  */
 void carregarKernel(void) {
 	int instrucao;
@@ -33,44 +32,8 @@ void carregarKernel(void) {
 	sim(instrucao, index);
 }
 
-/**
- * Parte principal da bios. Este programa carrega o Sistema Operacional e realiza algumas
- * checagens de componentes da máquina alvo. Não são adicionados offsets na MMU para o SO
- * pois o mesmo tem prioridade de execução e não necessita de offsets, uma vez que SEMPRE
- * o SO deverá estar armazenado no início da memória de instruções e utilizar também o 
- * início da memória de dados.
- */
+
 void main(void) {		
 	checarComponentes();
 	carregarKernel();
 }
-
-/**
- * ldk(address)
- *
- * RS - Address
- * RT - Register Destiny
- *
- * returns a instruction of HD
- */
-
-/**
- * sim(data, address)
- * 
- * RS - Address
- * RT - Data
- */
-
-/**
- * mmuLowerIM(data, selector)
- * 
- * RS - Selector
- * RT - Data
- */
-
-/**
- * mmuUpperIM(data, selector)
- * 
- * RS - Selector
- * RT - Data
- */
